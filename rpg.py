a0d8e0d747bb75bf5ab653478ca689c872445523
adityashah-codes <adityaahun@gmail.com>
2026-07-04 18:42:00 +0530

    Added critical hits and stamina mechanics to RPG

--- rpg.py
+++ rpg.py
@@ -1,13 +1,16 @@
 import random
 # variables
 player_hp = 100
+stamina = 50
 goblin_hp = 50
 portions = 3
-# damge dealer
-def dmg(min, max):
+# functions
+def dmg(min, max, crt):
     hit = random.randint(min, max)
-    return hit
-# portion dealer
+    if random.randint(1, 100) == crt:
+        hit = hit*2
+        return hit, True
+    return hit, False
 def heal():
     global player_hp, portions
     if portions > 0:
@@ -17,34 +20,62 @@ def heal():
         if  player_hp > 100:
             player_hp = 100
         print(f"You used 1 portion \n current HP {player_hp}")
+        print(f"You have {portions} left")
     else:
         print("You ran out of portions")
 # battle loop
 while player_hp > 0 and goblin_hp > 0:
     print("----BATTLE BEGINS----")
-    action = input("Enter 'A' to HIT the GOblin\nEnter 'P' to use Portion \n----> ")
-    if action.upper() == "A":
-        dmg_to_gob = dmg(10, 18)
-        goblin_hp = goblin_hp - dmg_to_gob
-        print(f"You hitted goblin : {dmg_to_gob}\nGoblin's Current HP : {goblin_hp} ")
-        if goblin_hp <= 0:
-            print("YOU DEFEATED GOBLIN")
-            break
+    action = input("Enter 'A' to HIT the GOblin\nEnter 'P' to use Portion\nEnter 'R' for rest \n----> ")
+    if action.upper() == "A": # hero's attack
+        if stamina > 10: # hero can attack
+            stamina = stamina - 15
+            dmg_to_gob, is_crt = dmg(10, 18, 20)
+            goblin_hp = goblin_hp - dmg_to_gob
+            if is_crt: # critical attack
+                print(f"BOOM!!, CRITICAL DEAlT 2x DAMAGE!!\n Damage Dealt {dmg_to_gob}\n Goblins Current HP : {goblin_hp}")
+            else: # normal attack
+                print(f"You hitted goblin and dealt damge {dmg_to_gob}\n Goblins Current HP : {goblin_hp}")
         else:
-            dmg_to_ply = dmg(3, 12)
-            player_hp = player_hp - dmg_to_ply
-            print(f"Goblin Bites you : {dmg_to_ply}\n Your current Health : {player_hp}")
+            print("You are out of stamina\nRest for a while (use 'R') : ")
+        # goblin's attack
+        dmg_to_hero , is_crt = dmg(5, 12, 10)
+        player_hp = player_hp - dmg_to_hero
         if player_hp <= 0:
-            print("Goblin defeated you")
-            break
-    elif action.upper() == "P":
+            print("Goblin won , Try again ;)")
+        else:
+            if is_crt: # goblin's critical attack
+                print(f"OOuch, Goblin Hitted You with a critical \n It dealt a damage of {dmg_to_hero}\n Your Current HP : {player_hp}")
+            else: # goblin's normal attack
+                print(f"Goblin Hitted you and dealt damage of {dmg_to_hero}\n Your Current HP : {player_hp}")
+    elif action.upper() == "P": # healing
         heal()
-        print(f"You have {portions} left")
-        dmg_to_ply = dmg(3, 12)
-        player_hp = player_hp - dmg_to_ply
-        print(f"Goblin Bites you : {dmg_to_ply}\n Your current Health : {player_hp}")
+        # goblin's attack
+        dmg_to_hero , is_crt = dmg(5, 12, 10)
+        player_hp = player_hp - dmg_to_hero
+        if player_hp <= 0:
+            print("Goblin won , Try again ;)")
+        else:
+            if is_crt: # goblin's critical attack
+                print(f"OOuch, Goblin Hitted You with a critical \n It dealt a damage of {dmg_to_hero}\n Your Current HP : {player_hp}")
+            else: # goblin's normal attack
+                print(f"Goblin Hitted you and dealt damage of {dmg_to_hero}\n Your Current HP : {player_hp}")
+    elif action.upper() == "R": # stamina recovery
+        stamina = stamina + 25
+        if stamina >= 50:
+            stamina = 50
+        print(f"You took a nap\n Your current stamina {stamina}")
+        # goblin's attack
+        dmg_to_hero , is_crt = dmg(5, 12, 10)
+        player_hp = player_hp - dmg_to_hero
+        if player_hp <= 0:
+            print("Goblin won , Try again ;)")
+        else:
+            if is_crt: # goblin's critical attack
+                print(f"OOuch, Goblin Hitted You with a critical \n It dealt a damage of {dmg_to_hero}\n Your Current HP : {player_hp}")
+            else: # goblin's normal attack
+                print(f"Goblin Hitted you and dealt damage of {dmg_to_hero}\n Your Current HP : {player_hp}")
     else:
-        print("INvalid Move")
+        print("Invalid Input\n Try 'A'")
 
-    
-    
+
